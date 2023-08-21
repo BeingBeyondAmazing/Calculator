9eb8b0aa894a2bae493c9ef89976f77724eba3be
BeingBeyondAmazing <welsh.beyond@gmail.com>
2023-08-21 12:55:42 -0400

    create operate funct

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let operand; let num1; let num2; let answer;
+function operate (num1, operand, num2){
+    if(operand === "+")
+    answer = add(num1, num2);
+    if(operand === "-")
+    answer = subtract(num1, num2);
+    if(operand === "*")
+    answer = multiply(num1, num2);
+    if(operand === "/")
+    answer = divide(num1, num2);
+}
+
 function add(num1, num2){
     return num1 + num2;
 }
