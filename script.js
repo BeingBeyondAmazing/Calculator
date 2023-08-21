87b372f4a65069ca2b33adb9fedbb0c542676e7b
BeingBeyondAmazing <welsh.beyond@gmail.com>
2023-08-21 19:55:00 -0400

    decimals work in cannot perform more than one operation at a time

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
-let operand; let num1; let num2; let answer;
+let operand = "" ; let num1 = ""; let num2 = "" ; let answer; let equation = "";
+
 const display = document.querySelector(".display");
-const buttons = document.querySelectorAll(".button");
-buttons.forEach(button => {
-    button.addEventListener('click', buttonClick)
+const nums = document.querySelectorAll("#num");
+nums.forEach(num => {
+    num.addEventListener('click', numClick)
 })
+
+const operands = document.querySelectorAll("#operand")
+operands.forEach(oper => {
+    oper.addEventListener('click', operandClick)
+})
+
+const equals = document.querySelector("#equals");
+equals.addEventListener("click", eqClick);
 
 
 function operate (num1, operand, num2){
     if(operand === "+")
-    answer = add(num1, num2);
+    answer = add(num1, num2).toFixed(2);
     if(operand === "-")
-    answer = subtract(num1, num2);
+    answer = subtract(num1, num2).toFixed(2);
     if(operand === "*")
-    answer = multiply(num1, num2);
+    answer = multiply(num1, num2).toFixed(2);
     if(operand === "/")
-    answer = divide(num1, num2);
+    answer = divide(num1, num2).toFixed(2);
+    return answer;
 }
-function buttonClick(event){
-    displayOut(event.target.textContent);
+
+function numClick(event){
+    if(operand !== ""){
+        num2 += event.target.textContent;
+        displayOut(num2);
+        return;
+    }
+    num1 += event.target.textContent;
+    displayOut(num1);
+}
+
+function operandClick(event){
+    if(operand !== ""){
+        num1 = 
+    }
+    operand = event.target.textContent;
+    displayOut(operand)
+}
+
+function eqClick(event){
+    equation = num1 + " " + operand + " "  + num2 + " " + "=" + " ";
+    answer = operate(parseFloat(num1),operand,parseFloat(num2));
+    displayOut(equation + answer);
+    num1 = answer; num2 = "";
 }
 
 function add(num1, num2){
